70ec5d275f0ae4610a314c6536b78f70521c5fe8
Joe Zucco <josephzucco@gmail.com>
2021-10-05 18:28:47 -0400

    establish boilerplate components

---
+++ src/components/Flashcard.js
@@ -0,0 +1,5 @@
+export const Flashcard = () => {
+  return <div></div>;
+};
+
+export default Flashcard;
---
+++ src/components/FlashcardList.js
@@ -0,0 +1,5 @@
+export const FlashcardList = () => {
+  return <div></div>;
+};
+
+export default FlashcardList;
