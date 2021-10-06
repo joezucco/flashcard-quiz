c21be4ea078b9acc6ca9c6bba96b2a56ba60cd1d
Joe Zucco <josephzucco@gmail.com>
2021-10-06 09:55:04 -0400

    create static dummy data

--- src/App.js
+++ src/App.js
@@ -2,4 +2,19 @@ function App() {
   return <h1>Hello World</h1>;
 }
 
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "What is 2 + 2",
+    answer: "4",
+    options: ["2", "3", "4", "5"],
+  },
+  {
+    id: 2,
+    question: "Question 2?",
+    answer: "Answer!",
+    options: ["Answer", "Answer 1", "Answer 2", "Answer 3"],
+  },
+];
+
 export default App;
