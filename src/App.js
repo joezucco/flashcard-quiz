1130bbe454176c9b9728e1198d7b0a1aff370df7
Joe Zucco <josephzucco@gmail.com>
2021-10-06 11:47:23 -0400

    complet basic build

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import "./app.css";
 import axios from "axios";
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  const [flashcards, setFlashcards] = useState([]);
   const [categories, setCategories] = useState([]);
 
   const categoryEl = useRef();
+  const amountEl = useRef();
 
   useEffect(() => {
     axios.get("https://opentdb.com/api_category.php").then((res) => {
@@ -15,25 +16,7 @@ function App() {
     });
   }, []);
 
-  useEffect(() => {
-    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
-      setFlashcards(
-        res.data.results.map((questionItem, index) => {
-          const answer = decodeStr(questionItem.correct_answer);
-          const options = [
-            ...questionItem.incorrect_answers.map((a) => decodeStr(a)),
-            answer,
-          ];
-          return {
-            id: `${index}-${Date.now()}`,
-            question: decodeStr(questionItem.question),
-            answer: answer,
-            options: options.sort(() => Math.random() - 0.5),
-          };
-        })
-      );
-    });
-  }, []);
+  useEffect(() => {}, []);
 
   const decodeStr = (str) => {
     const textArea = document.createElement("textarea");
@@ -43,7 +26,30 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //
+    axios
+      .get("https://opentdb.com/api.php", {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value,
+        },
+      })
+      .then((res) => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeStr(questionItem.correct_answer);
+            const options = [
+              ...questionItem.incorrect_answers.map((a) => decodeStr(a)),
+              answer,
+            ];
+            return {
+              id: `${index}-${Date.now()}`,
+              question: decodeStr(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - 0.5),
+            };
+          })
+        );
+      });
   };
 
   return (
@@ -61,6 +67,20 @@ function App() {
             })}
           </select>
         </div>
+        <div className='form-group'>
+          <label htmlFor='amount '>Number of Questions</label>
+          <input
+            type='number'
+            id='amount'
+            min='1'
+            step='1'
+            defaultValue={10}
+            ref={amountEl}
+          />
+        </div>
+        <div className='form-group'>
+          <button className='btn'>Generate</button>
+        </div>
       </form>
       <div className='container'>
         <FlashcardList flashcards={flashcards} />
@@ -68,20 +88,5 @@ function App() {
     </>
   );
 }
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "What is 2 + 2",
-    answer: "4",
-    options: ["2", "3", "4", "5"],
-  },
-  {
-    id: 2,
-    question: "Question 2?",
-    answer: "Answer!",
-    options: ["Answer", "Answer 1", "Answer 2", "Answer 3"],
-  },
-];
 
 export default App;
