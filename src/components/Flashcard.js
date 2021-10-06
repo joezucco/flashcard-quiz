60e361719a3be7672f39dedad5aefdd146102f71
Joe Zucco <josephzucco@gmail.com>
2021-10-06 10:13:22 -0400

    set conditional dynamic class on card

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -2,7 +2,14 @@ import { useState } from "react";
 
 export const Flashcard = ({ flashcard }) => {
   const [flip, setFlip] = useState(false);
-  return <div onClick={() => setFlip(!flip)}>{flip ? flashcard.answer : flashcard.question}</div>;
+  return (
+    <div
+      className={`card ${flip ? "flip" : ""}`}
+      onClick={() => setFlip(!flip)}
+    >
+      {flip ? flashcard.answer : flashcard.question}
+    </div>
+  );
 };
 
 export default Flashcard;
